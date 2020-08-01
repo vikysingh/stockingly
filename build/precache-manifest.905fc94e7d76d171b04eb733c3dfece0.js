cd32d6d8ab6d825d66334ede1d5c07a5055c4724
Vikram Singh <51959049+vikysingh@users.noreply.github.com>
2020-08-01 10:54:31 +0200

    Add files via upload

---
+++ build/precache-manifest.905fc94e7d76d171b04eb733c3dfece0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4adc83ba57a7d49ecc6cb09e4dea108",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d06ce2aad65a2875a287",
+    "url": "/static/css/4.48b28679.chunk.css"
+  },
+  {
+    "revision": "46320183433bda715c5f",
+    "url": "/static/css/5.922d0881.chunk.css"
+  },
+  {
+    "revision": "ea0b15c3f84ee6425aee",
+    "url": "/static/css/main.0b460682.chunk.css"
+  },
+  {
+    "revision": "bacbced90375522ec209",
+    "url": "/static/js/2.7e310e7d.chunk.js"
+  },
+  {
+    "revision": "bb07bf585b5364cfbb4632dd9308d60e",
+    "url": "/static/js/2.7e310e7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1845fe2286ddfea550a",
+    "url": "/static/js/3.c9944ec9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.c9944ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d06ce2aad65a2875a287",
+    "url": "/static/js/4.9aa8b566.chunk.js"
+  },
+  {
+    "revision": "46320183433bda715c5f",
+    "url": "/static/js/5.a4ecaeb6.chunk.js"
+  },
+  {
+    "revision": "ea0b15c3f84ee6425aee",
+    "url": "/static/js/main.641c5076.chunk.js"
+  },
+  {
+    "revision": "bd4b05df3b7e20f37550",
+    "url": "/static/js/runtime-main.5efebdc6.js"
+  }
+]);
